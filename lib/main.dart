13b42baffd190229c09fd5da3682d99d2136ec9e
Dirach4 <asrahman411@gmail.com>
2022-11-07 16:38:59 +0700

    Commid Ketiga

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Air Terjun Ngelirip',
       theme: ThemeData(),
       home: const DetailScreen(),
